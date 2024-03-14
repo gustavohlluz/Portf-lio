2b9de9edb9b836dd308d277c5d3f20e158f2f0cb
Gustavo Luz <gustavohlluz2@gmail.com>
2024-03-14 00:36:43 -0300

    Header fix

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,5 +1,8 @@
 
 import portfolioImg from "../../src/assets/portfolio.png"
+import { AboutMeSection } from "../sections/AboutMeSection";
+import { ProjectSection } from "../sections/ProjectsSection";
+import { TechSection } from "../sections/TechSection";
 import styles from "./style.module.css";
 
 export const Header = () => {
@@ -10,9 +13,9 @@ export const Header = () => {
                 <div>
                     <img src={portfolioImg} alt="Logo Portfolio" />
                     <nav>
-                        <a href="#">Sobre</a>
-                        <a href="#">Stack</a>
-                        <a href="#">Projetos</a>
+                        <a href={<AboutMeSection/>}>Sobre</a>
+                        <a href={<TechSection/>}>Stack</a>
+                        <a href={<ProjectSection/>}>Projetos</a>
                     </nav>
                     <button>Contato</button>
                 </div>
